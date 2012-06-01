53b64866fb4a579d56483721a6a50add5915607a
Abdelrahman Magdy <obad2015@gmail.com>
2012-06-02 00:56:28 +0200

    inital commit

---
+++ sites/all/modules/carpooling/misc/how_it_works.tpl.php
@@ -0,0 +1,34 @@
+<html>
+
+<div id='steps' align="center">
+
+	<div id='step1'></div>
+	<div id='step2'></div>
+	<div id='step3'></div>
+	<div id='step4'></div>
+	<div id='step5'></div>
+	<div class='clear'></div>
+
+	<div id='links'>
+
+		<div class='link'>
+		<?php echo l(t('Add Ride'),'node/add/ride') ?></div>
+		<div class='link'>
+		<?php echo l(t('Contact Me'),'contact') ?></div>
+		<div class='link'>
+		<?php echo l(t('Reserve'),'reserve') ?></div>
+		<div class='link'>
+		<?php echo l(t('Approved ?'),'approve') ?></div>
+		<div class='link'>
+		<?php echo l(t('Evaluate'),'evaluate') ?></div>
+
+
+		<div class="clear"></div>
+	</div>
+
+</div>
+
+
+
+
+</html>
---
+++ sites/all/modules/carpooling/misc/js/misc.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+  jQuery('form.node-form .form-radios, form.node-form .form-checkboxes').addClass('container-inline');
+});
