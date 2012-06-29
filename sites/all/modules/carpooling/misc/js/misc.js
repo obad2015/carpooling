a1c86cc88023cb79d4d06fcd627e1dba10fdd515
Abdelrahman Magdy <obad2015@gmail.com>
2012-06-30 01:54:47 +0200

    adding better search mechanism code, and ride requets

--- sites/all/modules/carpooling/misc/js/misc.js
+++ sites/all/modules/carpooling/misc/js/misc.js
@@ -7,6 +7,9 @@ jQuery(document).ready(function($) {
   
   jQuery('#edit-field-from-province-tid').change(update_from_cities);
   jQuery('#edit-field-to-province-tid').change(update_to_cities);
+  
+  // remove search rides attachment headers
+  jQuery('.view-search-rides .attachment thead').remove();
 });
 
 function update_from_cities() {
@@ -20,17 +23,31 @@ function update_to_cities() {
 function update_province_city_dependency(province_selector, city_selector) {
 	tid = jQuery(province_selector).val();
 	jQuery(city_selector).attr('disabled','disabled');
+	
 
 	if (tid != 'All') {
 		jQuery.getJSON(Drupal.settings.basePath + 'misc/province/cities/' + tid, {},  
-	    function (cities) {	  
+	    function (cities) {
+		  currOption = jQuery(city_selector).val();
+		  
 	      jQuery(city_selector + ' option').remove();
 	      jQuery.each(cities, function(key, value) {
 	        jQuery(city_selector)
 	        .prepend(jQuery('<option>', { value : key })
 	          .text(value)); 
 	      });
+	      
+	      var hasOption = jQuery(city_selector + ' option[value="' + currOption + '"]');
+	      
+	      if (hasOption.length != 0) {
+	    	  jQuery(city_selector).val(currOption);  
+	      }
+	      
+	      
 	      jQuery(city_selector).removeAttr('disabled');
 	    }); 
+	}
+	else {
+		jQuery(city_selector).val('All');
 	}
 }
